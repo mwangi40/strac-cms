ba6b5ac62fc90c78f854be2233cf28fc02e23c88
mwangi40 <edumwas052@gmail.com>
2025-06-24 10:27:10 +0300

    initial commit

---
+++ config/plugins.js
@@ -0,0 +1,13 @@
+console.log("🔧 Cloudinary config loaded");
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+    },
+  },
+});
